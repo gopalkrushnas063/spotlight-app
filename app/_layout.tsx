7eaf8a05b97205b570ee5148dfa45be3ca86ac8c
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2025-05-28 16:47:24 +0530

    auth logic written for route properly if user loggedin already

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
+import InitialLayout from "@/components/initialLayout";
 import { COLORS } from "@/constants/theme";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
@@ -9,11 +9,7 @@ export default function RootLayout() {
     <ClerkProvider tokenCache={tokenCache}>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-            }}
-          />
+          <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
     </ClerkProvider>
