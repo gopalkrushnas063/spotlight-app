17d6b9566b1e1309413493bf7eea86bd6cbea124
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2025-05-30 21:16:51 +0530

    schema created for backend operation using clerk and convex

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,16 @@
 import InitialLayout from "@/components/initialLayout";
 import { COLORS } from "@/constants/theme";
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import ClerkAndConvexProviders from "@/providers/ClerkAndConvexProviders";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkAndConvexProviders>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProviders>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,13 @@
+
+
+//https://easy-ocelot-44.clerk.accounts.dev
+
+
+export default {
+  providers: [
+    {
+      domain: "https://easy-ocelot-44.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+}; 
